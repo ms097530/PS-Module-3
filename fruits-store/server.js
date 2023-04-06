aad5f7594a587b75210f4cf150d3343a32f4f19e
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-06 15:21:34 -0400

    added comments

--- fruits-store/server.js
+++ fruits-store/server.js
@@ -31,6 +31,7 @@ app.get('/fruits', (req, res) =>
     res.send(fruits)
 })
 
+// show route
 app.get('/fruits/:indexOfFruitsArray', (req, res) =>
 {
     const { indexOfFruitsArray: index } = req.params
