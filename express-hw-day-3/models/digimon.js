91093f5e8bc347cdceaf58770855d36004a87b3e
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-07 15:47:16 -0400

    added data for hw

---
+++ express-hw-day-3/models/digimon.js
@@ -0,0 +1,44 @@
+const digimon = [
+    {
+        id: 1,
+        name: 'Agumon'
+    },
+    {
+        id: 2,
+        name: 'Greymon'
+    },
+    {
+        id: 3,
+        name: 'Metal Greymon'
+    },
+    {
+        id: 4,
+        name: 'War Greymon'
+    },
+    {
+        id: 5,
+        name: 'Gabumon'
+    },
+    {
+        id: 6,
+        name: 'Garurumon'
+    },
+    {
+        id: 7,
+        name: 'Were Garurumon'
+    },
+    {
+        id: 8,
+        name: 'Wormmon'
+    },
+    {
+        id: 9,
+        name: 'Patamon'
+    },
+    {
+        id: 10,
+        name: 'Angemon'
+    }
+]
+
+module.exports = digimon
