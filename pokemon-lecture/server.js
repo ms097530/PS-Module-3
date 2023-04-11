46263bdc53a9cd9c86ae5b07fa69f0a747afc3c4
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-11 10:09:49 -0400

    initialized pokemon app

--- pokemon-lecture/server.js
+++ pokemon-lecture/server.js
@@ -1,7 +1,18 @@
 const express = require('express')
+require('dotenv').config()
 
 const PORT = 3000
 const app = express()
+
+// const username = process.env.MONGO_USERNAME
+// const pw = process.env.MONGO_PW
+// const connectionURI = `mongodb+srv://${username}:${pw}@pokemon-1.1vc82sb.mongodb.net/?retryWrites=true&w=majority`
+
+const mongoURI = process.env.MONGO_URI
+
+app.set('views', 'views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('jsx-view-engine').createEngine())
 
 app.listen(PORT, () =>
 {
