71004a03af72330156680199f0051dc14c09a353
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-12 13:52:28 -0400

    added Tweet model

---
+++ intro-to-mongoose/models/Tweet.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const tweetSchema = new mongoose.Schema(
+    {
+        title: String,
+        body: {
+            type: String,
+            required: true,
+            maxLength: 255
+        },
+        author: String,
+        category: {
+            type: String,
+            enum: ['Programming', 'Gaming', 'Art']
+        },
+        likes: {
+            type: Number,
+            default: 0
+        },
+        sponsored: {
+            type: Boolean,
+            default: false
+        }
+    },
+    // adds timestamps to these values when created (and edited?)
+    { timestamps: true }
+)
+
+const Tweet = mongoose.model('Tweet', tweetSchema)
+
+module.exports = Tweet
