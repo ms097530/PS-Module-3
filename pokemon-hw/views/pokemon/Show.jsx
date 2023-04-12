7b0a9672cb0b4b0e120fcf435851439457ed0cd3
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-12 11:33:20 -0400

    updated output

--- pokemon-hw/views/pokemon/Show.jsx
+++ pokemon-hw/views/pokemon/Show.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Show({ pokemon })
 {
+
     return (
         <div>
+            <h2>#{pokemon.pokedexId}</h2>
             <h1>{pokemon.name.toUpperCase()}</h1>
+            <p>Types: {pokemon.types.map(type => <span>{type} </span>)}</p>
             <div>
                 <a href="/pokemon">Go Home</a>
             </div>
