67ca899e41313635d1c110c2569a4d6efe3b2948
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-13 11:54:24 -0400

    cleaned up return statement and formatted date output to avoid error

--- mongoose-flights/views/flights/user/Index.jsx
+++ mongoose-flights/views/flights/user/Index.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 
 export default function Index({ flights })
 {
+    console.log(flights)
+
+    const result = flights.data ?
+        flights.data.map((flight) =>
+            <div key={flight._id}>
+                <h2>{flight.flightNo}</h2>
+                <h3>{flight.airline}</h3>
+                <h3>{flight.departs.toString()}</h3>
+            </div>
+        )
+        : <h2>No flights found</h2>
+
+
     return (
         <div>
             <nav>
                 <a href="/flights/new">Book new flight</a>
             </nav>
             <h1>Flights</h1>
+            {result}
             {
-                flights ?
-                    flights.map((flight) =>
-                        <div>
-                            <h2>{flight.flightNo}</h2>
-                            <h3>{flight.airline}</h3>
-                            <h3>{flight.departs}</h3>
-                        </div>
-                    )
-                    : <h2>No flights found</h2>
+                flights.error && <h3>Some error occurred...</h3>
             }
         </div>
     )
