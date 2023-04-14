f42e77d0879d846dabb1fd157f5b33ca369f102d
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-14 15:51:01 -0400

    setup seed data file and route

--- captains-log/server.js
+++ captains-log/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
+const fs = require('fs').promises
 
 const connectToDB = require('./config/db')
+const Log = require('./models/Log')
 
 const PORT = 3000
 const app = express()
@@ -23,16 +25,25 @@ app.get('/', (req, res) =>
     res.redirect('/logs')
 })
 
+/**
+ * * INDEX ROUTE
+ */
 app.get('/logs', (req, res) =>
 {
     res.send('LOGS')
 })
 
+/**
+ * * NEW FORM ROUTE
+ */
 app.get('/logs/new', (req, res) =>
 {
     res.render('logs/New')
 })
 
+/**
+ * * CREATE ROUTE
+ */
 app.post('/logs', (req, res) =>
 {
     req.body.shipIsBroken = req.body.shipIsBroken === 'on' ? true : false
@@ -40,10 +51,27 @@ app.post('/logs', (req, res) =>
     res.send(req.body)
 })
 
+/**
+ * * SEED ROUTE
+ */
+app.get('/logs/seed', async (req, res) =>
+{
+    // using readFile from fs promises
+    const buf = await fs.readFile('./seed/data.json')
+    // console.log(buf.toString())
+    // convert buffer to string and parse the JSON to usable data
+    const seedData = JSON.parse(buf.toString())
+    const result = await Log.insertMany(seedData)
+    console.log(result)
+    res.redirect('/logs')
+})
+
 app.get('*', (req, res) =>
 {
     res.send('FALLBACK ROUTE')
 })
+
+
 
 app.listen(PORT, () =>
 {
