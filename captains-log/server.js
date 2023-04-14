b0c730c63a7ba7e931b66689a8164a3374b65fad
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-14 14:02:52 -0400

    initialized captain's log lab

---
+++ captains-log/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+
+const PORT = 3000
+const app = express()
+
+app.listen(PORT, () =>
+{
+    console.log(`Listening on port ${PORT}`)
+})
