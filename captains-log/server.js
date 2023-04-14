09f065e5d17194c5795c5ab5f30a24e31a4cdaba
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-14 14:45:30 -0400

    added New logs route and view

--- captains-log/server.js
+++ captains-log/server.js
@@ -4,6 +4,30 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
+app.set('views', 'views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('jsx-view-engine').createEngine())
+
+app.get('/', (req, res) =>
+{
+    res.redirect('/logs')
+})
+
+app.get('/logs', (req, res) =>
+{
+    res.send('LOGS')
+})
+
+app.get('/logs/new', (req, res) =>
+{
+    res.render('logs/New')
+})
+
+app.get('*', (req, res) =>
+{
+    res.send('FALLBACK ROUTE')
+})
+
 app.listen(PORT, () =>
 {
     console.log(`Listening on port ${PORT}`)
---
+++ captains-log/views/logs/New.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+module.exports = function New()
+{
+    return (
+        <div>
+            <form action="/logs" method="post">
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" name="title" id="title" />
+                </div>
+                <div>
+                    <label htmlFor="entry">Entry:</label>
+                    <textarea name="entry" id="entry" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label htmlFor="shipIsBroken">Ship broken:</label>
+                    <input type="checkbox" name="shipIsBroken" id="shipIsBroken" />
+                </div>
+                <input type="submit" value="Submit Log" />
+            </form>
+        </div>
+    )
+}
